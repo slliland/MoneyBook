24849f09c5b4391ae926d9b977e66fb78530654a
yujian <645113027@qq.com>
2024-10-25 10:31:22 -0400

    feat: Modify vercel.json

--- server.js
+++ server.js
@@ -12,19 +12,19 @@ const dbData = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json'), 'utf-
 const root = path.join(__dirname, 'build');
 app.use(express.static(root, { maxAge: 86400000 }));
 
-// API route to get items based on monthCategory query
+// Endpoint to get items by monthCategory
 app.get('/api/items', (req, res) => {
   const monthCategory = req.query.monthCategory;
   const filteredItems = dbData.items.filter(item => item.monthCategory === monthCategory);
   res.json(filteredItems);
 });
 
-// API route to get all categories
+// Endpoint to get all categories
 app.get('/api/categories', (req, res) => {
   res.json(dbData.categories);
 });
 
-// Catch-all route to serve React app for other paths
+// Catch-all route to serve the React app for any other path
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(root, 'index.html'));
 });
